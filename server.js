9ffd5d824738ed7ab27b898bf9944d0d5219d5ff
ElyriaG <ElyriaG@users.noreply.github.com>
2019-06-22 10:45:58 -0500

    server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+
+var express = require('express');
+var app = express();
+var port = process.env.PORT || 9000;
+var config = require("./config");
+
+app.use(express.static(__dirname + '/views'));
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+app.listen(port);
+
+app.route('/').get(function(req, res){
+    res.json(config);
+})
