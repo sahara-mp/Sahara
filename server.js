d633c6539fd45074518d7d0ba82a7d05fafbee85
ElyriaG <ElyriaG@users.noreply.github.com>
2019-06-22 13:33:45 -0500

    Basic handlebars layout

--- server.js
+++ server.js
@@ -1,16 +1,26 @@
 require("dotenv").config();
+const colors = require("colors");
 
 var express = require('express');
 var app = express();
-var port = process.env.PORT || 9000;
+var PORT = process.env.PORT || 9000;
 var config = require("./config");
+let exphbs = require("express-handlebars");
+
 
 app.use(express.static(__dirname + '/views'));
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 
-app.listen(port);
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-app.route('/').get(function(req, res){
-    res.json(config);
-})
+
+app.listen(PORT, function() {
+    console.log(colors.rainbow("Server listening on: http://localhost:" + PORT));
+  });
+  
+
+// app.route('/').get(function(req, res){
+//     res.json(config);
+// })
